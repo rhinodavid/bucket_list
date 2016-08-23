d7c8e0f6841093dde65225e7547b875b45c13d89
David Walsh <dawalsh@gmail.com>
2016-08-23 00:31:50 -0400

    Implement server connection
    Server is returning 500 when trying to create a user.
    Suspect it is because the password hash was longer than the
    45 char limit.

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, json, request
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
+
+mysql = MySQL()
+
+# MySql configuration
+app.config['MYSQL_DATABASE_USER'] = 'bucketlist'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'bucketlist'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def main():
@@ -16,7 +28,16 @@ def signUp():
   _password = request.form['inputPassword']
 
   if _name and _email and _password:
-    return json.dumps({'html':'<span>All fields good!!</span>'})
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    _hashed_password = generate_password_hash(_password)
+    cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+    data = cursor.fetchall()
+    if len(data) is 0:
+      conn.commit()
+      return json.dumps({'message':'User created successfully'})
+    else:
+      return json.dumps({'error':str(data[0])})
   else:
     return json.dumps({'html':'<span>Enter the required fields</span>'})
 
