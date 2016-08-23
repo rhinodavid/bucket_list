ddb94ea3f2026d356ae959cf97e85d4e61fc540c
David Walsh <dawalsh@gmail.com>
2016-08-23 10:22:27 -0400

    Modify signup to error check and close cursor and connection

--- app.py
+++ app.py
@@ -23,23 +23,31 @@ def showSignUp():
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
-  _name = request.form['inputName']
-  _email = request.form['inputEmail']
-  _password = request.form['inputPassword']
+  try:
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
 
-  if _name and _email and _password:
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    _hashed_password = generate_password_hash(_password)
-    cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-    data = cursor.fetchall()
-    if len(data) is 0:
-      conn.commit()
-      return json.dumps({'message':'User created successfully'})
+    if _name and _email and _password:
+      conn = mysql.connect()
+      cursor = conn.cursor()
+      _hashed_password = generate_password_hash(_password)
+      cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+      data = cursor.fetchall()
+      if len(data) is 0:
+        conn.commit()
+        return json.dumps({'message':'User created successfully'})
+      else:
+        return json.dumps({'error':str(data[0])})
     else:
-      return json.dumps({'error':str(data[0])})
-  else:
-    return json.dumps({'html':'<span>Enter the required fields</span>'})
+      return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+  except Exception as e:
+    return json.dumps({'error':str(e)})
+
+  finally:
+    cursor.close()
+    conn.close()
 
 @app.route('/showSignIn')
 def showSignin():
